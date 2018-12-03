b0ef72d6aba1a75b391d043c0fd7016589dd596b
Giorgi Lomidze <gio.lomidze@gmail.com>
2018-12-03 18:47:39 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import AddEntry from './components/AddEntry';
+import { getMetricMetaInfo } from './utils/helpers';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <AddEntry />
+      </View>
+    );
+  }
+}
