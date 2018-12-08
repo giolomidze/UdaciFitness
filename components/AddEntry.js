4c1856d00ef6d039f4d5cdcf765b54a1b5664552
Giorgi Lomidze <gio.lomidze@gmail.com>
2018-12-08 11:20:39 -0500

    feat: complete read vs react-native

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
-import { getMetricMetaInfo, timeToString } from '../utils/helpers';
+import {
+  getMetricMetaInfo,
+  timeToString,
+  getDailyReminderValue,
+} from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
 import { Ionicons } from '@expo/vector-icons';
 import TextButton from './TextButton';
+import { submitEntry, removeEntry } from '../utils/api';
+import { connect } from 'react-redux';
+import { addEntry } from '../actions';
 
 function SubmitBtn({ onPress }) {
   return (
@@ -15,7 +22,7 @@ function SubmitBtn({ onPress }) {
   );
 }
 
-export default class AddEntry extends Component {
+class AddEntry extends Component {
   state = {
     run: 0,
     bike: 0,
@@ -54,24 +61,32 @@ export default class AddEntry extends Component {
     const key = timeToString();
     const entry = this.state;
 
-    // Update Redux
+    this.props.dispatch(
+      addEntry({
+        [key]: entry,
+      })
+    );
 
     this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }));
 
     // Navigate to home
 
-    // Save to "DB"
+    submitEntry({ key, entry });
 
     // Clear local notification
   };
   reset = () => {
     const key = timeToString();
 
-    // Update Redux
+    this.props.dispatch(
+      addEntry({
+        [key]: getDailyReminderValue(),
+      })
+    );
 
     // Route to Home
 
-    // Update "DB"
+    removeEntry(key);
   };
   render() {
     const metaInfo = getMetricMetaInfo();
@@ -79,7 +94,7 @@ export default class AddEntry extends Component {
     if (this.props.alreadyLogged) {
       return (
         <View>
-          <Ionicons name={'ios-happy'} size={100} />
+          <Ionicons name={'ios-happy-outline'} size={100} />
           <Text>You already logged your information for today.</Text>
           <TextButton onPress={this.reset}>Reset</TextButton>
         </View>
@@ -118,3 +133,13 @@ export default class AddEntry extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  const key = timeToString();
+
+  return {
+    alreadyLogged: state[key] && typeof state[key].today === 'undefined',
+  };
+}
+
+export default connect(mapStateToProps)(AddEntry);
